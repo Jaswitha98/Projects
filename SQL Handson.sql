6d8fe976fca5be8a5cf700b56f2a33c82a11955e
Jaswitha98 <jaswitha99@gmail.com>
2022-01-07 12:44:54 +0530

    Added avg

--- SQL Handson.sql
+++ SQL Handson.sql
@@ -1,20 +1,23 @@
-create database assign
-create table Sales
+create table customer
 (
-SALESMAN_ID numeric(5),
-NAME varchar(30),
-CITY varchar(15),
-COMMISION decimal(5,2)
-)
+ customer_id  numeric(5),
+ cust_name varchar(30),
+ city varchar(15), 
+ grade numeric(3)null, 
+ salesman_id numeric(5)
+ )
+ insert into customer values(3002,'Nick Rimando','New York',100,5001)
+ insert into customer values(3007,'Brad Davis', 'New York',200,5001)
+ insert into customer values(3005,'Graham Zusi','California',200,5002)
+ insert into customer values(3008,'Julian Green','London',300,5002)
+ insert into customer values(3004,'Fabian Johnson','Paris',300,5006)
+ insert into customer values(3009,'Geoff Cameron','Berlin',100,5003)
+ insert into customer values(3003,'Jozy Altidor','Moscow',200,5007)
+ insert into customer values(3001,'Brad Guzan','London',200,5005)
 
-insert into Sales values(5001,'James Hoog','New York', 0.15)
-insert into Sales values(5002,'Nail Knite','Paris',0.13)
-insert into Sales values(5005,'Pit Alex','London', 0.11)
-insert into Sales values(5006,'Mc Lyon ','Paris',0.14)
-insert into Sales values(5007,'Paul Adam','Rome', 0.13)
-insert into Sales values(5003,'Lauson Hen','San Jose',0.12)
-
-select * from Sales
-select NAME,COMMISION from Sales
-select  NAME,CITY  from Sales where CITY='Paris'
-select count(*) as 'count' from Sales
+ select * from customer
+ select customer_id,cust_name,city,grade,salesman_id from customer where grade=200
+ 
+select count(*) as 'count' from customer
+select * from customer
+select city,max(grade) as 'max' from customer group by city
